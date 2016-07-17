336bc566b2f154bb9a54021560d4db8400bc2107
Daniel Siwek <danielsiwek95@gmail.com>
2016-07-17 15:06:17 +0200

    Improve code, add bullet action

--- script.js
+++ script.js
@@ -1,15 +1,65 @@
 "use strict";
 
-$("span:even").css('color','red');
+$(function(){
 
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
+	var carouselList = $("#carousel ul");
+	var currentIndex = 0;
+	var previousIndex = -1;
 
-	var button = '<button class="btn" data-tmp="' + index + '">Click me</button>'
-	$(element).append(button) 
+	$('ul li img').each(function(index) {
+		var bullet = `<span class="bullet" id="${index}"></span>`;
+		$('.bullets').append(bullet);
+	})
 
-});
+	applyClass();
 
-$("button").click(function(){
-	alert($(this).attr("data-tmp"));
+	function changeSlide(direction = 'right', speed = 500){
+		var marginLeft = direction === 'right' ? '-100vw' : '+100vw';
+
+		window.clearInterval(interval);
+		interval = setInterval(changeSlide, 3000);
+
+		carouselList.animate({marginLeft}, speed, moveFirstSlide);
+		previousIndex = currentIndex;
+		direction === 'right' ? currentIndex++ : currentIndex--;
+		if (currentIndex > $(".bullet").length-1) currentIndex = 0;
+		if (currentIndex < 0) currentIndex = 3;
+		applyClass();
+	}
+	var interval = setInterval(changeSlide, 3000);
+
+	function moveFirstSlide() {
+
+		var firstItem = carouselList.find("li:first");
+		var lastItem = carouselList.find("li:last");
+
+		lastItem.after(firstItem)
+		carouselList.css({marginLeft:0});
+
+	}
+
+	function applyClass() {
+		document.getElementById(currentIndex).style.backgroundColor = '#000';
+		(previousIndex > -1) ? document.getElementById(previousIndex).style.backgroundColor = '#fff' : null;
+	}
+
+	$(".control").click(function(el){
+		var direction = el.target.id;
+
+
+
+		changeSlide(direction);
+	});
+
+	$(".bullet").click(function (el) {
+		var id = el.target.id;
+		var times = id - currentIndex;
+		var direction = times < 0 ? 'left' : 'right';
+
+		for (var i = 0; i < Math.abs(times); i++) {
+			changeSlide(direction, 100);
+		}
+
+	})
+
 });
